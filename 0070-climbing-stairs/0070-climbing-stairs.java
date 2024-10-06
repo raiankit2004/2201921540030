adf1aa4518b1b8629a4d19df809a16b21702038a
raiankit2004 <raiankit2004@gmail.com>
2024-10-07 00:19:11 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (11.88%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+      if(n==1)
+      {
+        return 1;
+      }  
+    int a[]= new int[n+1];
+      a[1]=1;
+      a[2]=2;
+      for(int i=3;i<=n;i++){
+        a[i] = a[i-1]+a[i-2]; 
+      }
+      return a[n];
+    }
+}
