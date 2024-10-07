cbff690409a9c78fc53c72be3d138df08f8b8f41
raiankit2004 <raiankit2004@gmail.com>
2024-10-08 01:13:46 +0530

    Time: 21 ms (53.42%), Space: 27.6 MB (50.74%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     void reverse(std::vector<int>& nums, int i, int j) {
         while (i < j) {
-            std::swap(nums[i], nums[j]);
+             int temp=nums[i];
+        nums[i]=nums[j];
+           nums[j]=temp;
             i++;
             j--;
         }
     }
     void rotate(vector<int>& nums, int k) {
          int n = nums.size();
-        // Ensure k is within the bounds of the array length
         k = k % n;
-        if (k == 0) return; // No need to rotate if k is 0
-
-        // Step 1: Reverse the entire array
         reverse(nums, 0, n - 1);
-        // Step 2: Reverse the first k elements
         reverse(nums, 0, k - 1);
-        // Step 3: Reverse the remaining n-k elements
         reverse(nums, k, n - 1);
     }
 };
