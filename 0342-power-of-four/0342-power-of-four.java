8586630a500823f32d4c712ce1a4933697c4344d
raiankit2004 <raiankit2004@gmail.com>
2024-10-28 09:14:40 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (88.95%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n > 0) {
+            if (n == 1) {
+                return true;
+            }
+            if (n % 4 != 0)
+                return false;
+            n = n / 4;
+        }
+        return true;
+    }
+}
