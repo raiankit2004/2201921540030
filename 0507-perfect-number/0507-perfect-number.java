e3673db318c7239665b7746dd766994e7fe6d4d9
raiankit2004 <raiankit2004@gmail.com>
2024-11-12 18:20:03 +0530

    Time: 2080 ms (64.95%), Space: 40.3 MB (46.02%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -2,9 +2,8 @@ class Solution {
     public boolean checkPerfectNumber(int num) {
         int count=0;
         for(int i=1;i<=num/2;i++){
-            if(num%i==0){
+            if(num%i==0)
             count=count+i;
-        }
         }
         if(count==num){
             return true;
