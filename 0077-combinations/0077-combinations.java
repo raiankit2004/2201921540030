1d70d5744dccac8c3d0ea7563b4ae991d41001d6
raiankit2004 <raiankit2004@gmail.com>
2024-11-12 21:13:23 +0530

    Time: 18 ms (81.63%), Space: 95 MB (12.13%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        solve(n,k,new ArrayList<>(),ans);
+        solve(1,n,k,new ArrayList<>(),ans);
    return ans;     
     }
-public void solve(int n,int k,List<Integer> curr,List<List<Integer>> ans){
+public void solve(int start,int n,int k,List<Integer> curr,List<List<Integer>> ans){
     if(k==0){
         ans.add(new ArrayList<>(curr));
         return;
@@ -13,8 +13,8 @@ public void solve(int n,int k,List<Integer> curr,List<List<Integer>> ans){
             return;
       }
         curr.add(n);
-        solve(n-1,k-1,curr,ans);
+        solve(1,n-1,k-1,curr,ans);
         curr.remove(curr.size()-1);
-        solve(n-1,k,curr,ans);
+        solve(1,n-1,k,curr,ans);
     }
 }
