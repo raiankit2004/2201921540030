cbb3e5d75f8da6c773eef2ffdf6c82ff9b1d1cbd
raiankit2004 <raiankit2004@gmail.com>
2025-03-26 20:39:43 +0530

    Time: 1 ms (99.43%), Space: 45.2 MB (60.82%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
-        int near = 0, far = 0, jumps = 0;
+        int jumps = 0, currentEnd = 0, farthest = 0;
 
-        while (far < nums.length - 1) {
-            int farthest = 0;
-            for (int i = near; i <= far; i++) {
-                farthest = Math.max(farthest, i + nums[i]);
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            
+            // When we reach the end of the current range
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
             }
-            near = far + 1;
-            far = farthest;
-            jumps++;
         }
-
-        return jumps;        
+        return jumps;
     }
 }
