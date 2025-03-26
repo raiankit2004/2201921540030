aeb044a65538006c29a396d4bb9b04a49878e465
raiankit2004 <raiankit2004@gmail.com>
2025-03-26 09:13:25 +0530

    Time: 1 ms (82.75%), Space: 44.2 MB (29.56%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -8,18 +8,16 @@ class Solution {
             nums[i+1]=0;
             i++;
         }
-    } 
-    int index = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                nums[index] = nums[i];
-                if (index != i) {
-                    nums[i] = 0;  
-                }
-                index++;  
-            }
-        }
-
+    }
+     int index=0;
+         for(int i=0;i<n;i++){
+      if(nums[i]!=0){
+        int temp = nums[index];
+        nums[index]=nums[i];
+        nums[i]=temp;
+        index++;
+      }
+    }
       return nums;
     }
 }
