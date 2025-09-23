9175f2b20dce36b1a04557507f4aed4dcf2ab108
raiankit2004 <raiankit2004@gmail.com>
2025-09-23 23:35:56 +0530

    Time: 0 ms (100%), Space: 41.5 MB (89.74%) - LeetHub

---
+++ 1858-latest-time-by-replacing-hidden-digits/1858-latest-time-by-replacing-hidden-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String maximumTime(String time) {
+      char res[]=time.toCharArray();
+      if(res[0]=='?')
+      res[0]=(res[1]<='3'||res[1]=='?')?'2':'1';
+
+      if(res[1]=='?')
+      res[1]=(res[0]=='2')?'3':'9';
+// second element res[2]=':';
+      if(res[3]=='?')
+      res[3]='5';
+
+       if(res[4]=='?')
+      res[4]='9';
+
+     return new String(res);
+    }
+}
