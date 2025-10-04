9522cc4a711f3b5e55ef1495ddd6bdd4ea7cc742
raiankit2004 <raiankit2004@gmail.com>
2025-10-04 12:57:29 +0530

    Time: 0 ms (100%), Space: 43 MB (50.13%) - LeetHub

--- 4009-bitwise-or-of-even-numbers-in-an-array/4009-bitwise-or-of-even-numbers-in-an-array.java
+++ 4009-bitwise-or-of-even-numbers-in-an-array/4009-bitwise-or-of-even-numbers-in-an-array.java
@@ -1,15 +1,11 @@
 class Solution {
     public int evenNumberBitwiseORs(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
         int res=0;
         for(int i:nums){
             if(i%2==0){
-                list.add(i);
+                res=res|i;
             }
         }
-          for(int i=0;i<list.size();i++){
-            res=res|list.get(i);
-          }
           return res;
     }
 }
