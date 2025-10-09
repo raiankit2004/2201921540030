56c9297490b3b14c7e02ed4484a499d355490872
raiankit2004 <raiankit2004@gmail.com>
2025-10-10 01:08:17 +0530

    Time: 4 ms (69.6%), Space: 45.1 MB (34.93%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int longestValidParentheses(String s) {
+        
+        if(s.length()==0)return 0;
+        int len=0;
+       Stack<Integer> st=new Stack<>();
+       st.push(-1);
+        for(int i=0;i<s.length();i++){
+           char ch=s.charAt(i);  // (
+
+            if(ch=='(') st.push(i); //  (
+
+                else{
+                    st.pop();  // ) if  this exist
+                    if(st.isEmpty()){
+                        st.push(i);
+                    }
+                    else{
+                        len=Math.max(len,i-st.peek());
+                    }
+                }
+        }
+          
+        return len;
+    }
+}
