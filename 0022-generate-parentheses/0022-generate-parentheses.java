f1633a3f5c2958398574da1782e206fc8997f799
raiankit2004 <raiankit2004@gmail.com>
2025-11-11 10:20:24 +0530

    Time: 1 ms (80.7%), Space: 44.3 MB (18.75%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,20 +1,24 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-      List<String> list=new ArrayList<>();
-      dfs(0,0,"",n,list);
-      return list;
-    } 
-    public void dfs(int open,int close,String s,int n,List<String> list){
-       if(open==close && open+close==n*2){
-         list.add(s);
-         return;
-       }
-       if(open<n)
-       dfs(open+1,close,s+"(",n,list);
-       if(close<open)
-       dfs(open,close+1,s+")",n,list);
-
+        List<String> result = new ArrayList<>();
+        genStr(result, new StringBuilder(), 0, 0, n);
+        return result;
+    }
+    public static void genStr(List<String> list, StringBuilder sb, int open, int close, int max){
+        if(sb.length()==max*2){
+            list.add(sb.toString());
+            return;
+        }
+        if(open<max){
+            sb.append("(");
+            genStr(list, sb, open+1, close, max);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if(close<open){
+            sb.append(")");
+            genStr(list, sb, open, close+1, max);
+            sb.deleteCharAt(sb.length()-1);
+        }
 
     }
-      
 }
