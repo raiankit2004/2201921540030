a44aae30cf2a76ce99ac0f45beddb4eb970f592e
raiankit2004 <raiankit2004@gmail.com>
2026-03-10 09:55:12 +0530

    Time: 0 ms (100%), Space: 41.4 MB (100%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -1,14 +1,5 @@
 class Solution {
     public int removePalindromeSub(String s) {
-        StringBuilder sb=new StringBuilder(s);
-        String rev="";
-        int count=0;
-        rev=sb.reverse().toString();
-        if(s.equals(rev)){
-            count=1;
-        }else{
-            count=2;
-        }
-        return count;
+        return  (s.equals(new StringBuilder(s).reverse().toString()) ? 1 : 2);
     }
 }
